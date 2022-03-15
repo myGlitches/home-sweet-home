d7782776ff9626b5819b30dc0b4c06e67b81d436
Suryadipta <gremlin.sarkar@gmail.com>
2022-03-15 23:55:12 +0530

    Create Listing Page created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,10 @@ import { getAuth, updateProfile } from "firebase/auth"
 import { useState } from "react"
 import { updateDoc, doc } from "firebase/firestore"
 import { db } from "../firebase.config"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg"
+import homeIcon from "../assets/svg/homeIcon.svg"
 
 function Profile() {
   const auth = getAuth()
@@ -90,6 +92,12 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or Rent your Home</p>
+          <img src={arrowRight} alt='arrow Right' />
+        </Link>
       </main>
     </div>
   )
